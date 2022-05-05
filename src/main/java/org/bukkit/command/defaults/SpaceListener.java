d2a0b26b69a6c02743fec1f508c49d96102f4f06
AnswerForMystery <jim20071128@163.com>
2022-05-05 22:19:08 +0800

    [+] Update core

--- src/main/java/org/bukkit/command/defaults/SpaceListener.java
+++ src/main/java/org/bukkit/command/defaults/SpaceListener.java
@@ -18,7 +18,7 @@ public class SpaceListener implements Listener {
         if (title.equals(color("&7[&b更多背包&7]"))){
             String data = System.getProperty("user.dir") + "/MoreSpaceData/";
             Player p = (Player) event.getPlayer();
-            File file = new File(data + p.getDisplayName());
+            File file = new File(data + p.getDisplayName() + ".yml");
             FileConfiguration config = YamlConfiguration.loadConfiguration(file);
             for (int i = 0;i<event.getInventory().getContents().length;i++){
                 config.set("items." + i,event.getInventory().getContents()[i]);
